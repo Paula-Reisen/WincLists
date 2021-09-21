b25d36b914e01fb866e2d3370061c87a435600ed
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2021-09-21 20:17:35 +0200

    Opdracht10

---
+++ Opdracht10/myfirstjavascript.js
@@ -0,0 +1,20 @@
+let firstName = 'Paula';
+// let if = 'als'; //Mag geen if statement gebruiken op deze locatie. If wordt gebruikt bij if.. then statements
+let number = 4 + 4;
+let countString = '4' + '4'
+let additionNumber = number + 16;
+let multiplyNumber = number * 3;
+let substractNumber = number - 9;
+let divisionNumber = number / 2;
+let modulusNumber = number % 4;
+let age = 31 + 'jaar';
+
+console.log(firstName);
+console.log(number);
+console.log(countString); //wordt niet opgeteld, maar achter elkaar gezet als 2 strings.
+console.log(additionNumber);
+console.log(multiplyNumber);
+console.log(substractNumber);
+console.log(divisionNumber);
+console.log(modulusNumber);
+console.log(typeof (age))
