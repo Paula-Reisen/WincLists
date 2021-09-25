3a36969ca111b00af150dd59f641e2b767141d7a
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2021-09-25 10:53:47 +0200

    Opdracht 11

---
+++ Opdracht11/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten");
+
+//Dit is een grote som
+/*let calculationMutiply = 1230941 * 1823794;
+console.log(CalculationMutiply);
+
+let calculateDivision = 637263 / 54;
+console.log(calculateDivision);*/
