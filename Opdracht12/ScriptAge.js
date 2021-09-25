a3353451db992134d79fe11e6e6152b4a5572f07
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2021-09-25 11:30:30 +0200

    Opdracht12

---
+++ Opdracht12/ScriptAge.js
@@ -0,0 +1,23 @@
+const age = 18;
+
+if (age >= 18) {
+    console.log("You can enter the bar!");
+} else {
+    console.log("I can't let you enter the bar.");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Your allowed to enter ladiesnight.");
+} else {
+    console.log("You are not a female so you can't enter ladiesnight.");
+}
+
+const driverStatus = 'job';
+
+if (driverStatus == 'bob') {
+    console.log("You are allowed to drive home.");
+} else {
+    console.log("You are not the bob so you are not allowed to drive home.");
+}
