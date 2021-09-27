49283fa903731de16be50f66b5753873eb5441ea
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2021-09-27 20:49:03 +0200

    opdracht15

---
+++ Opdracht15/Functions.js
@@ -0,0 +1,24 @@
+const makingSushi = function () {
+    console.log('Cooking rice')
+    console.log('Draining rice')
+    console.log('Rolling sushi')
+    console.log('add additional sauce')
+};
+
+makingSushi();
+makingSushi();
+makingSushi();
+
+const makingPizza = function () {
+    console.log('Making pizza dough')
+    console.log('Putting the sauce on dough')
+    console.log('Put vegetables and meat on pizza')
+    console.log('Putting the cheese on top')
+    console.log('Baking the pizza')
+};
+makingPizza();
+const addAnotherPizza = makingPizza;
+addAnotherPizza();
+
+
+
