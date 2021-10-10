3f99e651e8494d0837fade75cbc6d32f775f5678
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2021-10-10 15:27:29 +0200

    Opdracht20

---
+++ Opdracht19/Solution.js
@@ -0,0 +1,27 @@
+//exercise vat 1
+const calculateVAT = function (basePrice, VATPercentage) {
+    return basePrice * (VATPercentage / 100);
+};
+
+const calculatePriceIncludingVAT = function (basePrice, VATPercentage) {
+    const VAT = calculateVAT(basePrice, VATPercentage);
+    return basePrice + VAT;
+};
+
+console.log(calculatePriceIncludingVAT(1000, 21)); // 1210
+console.log(calculatePriceIncludingVAT(2, 9)); // 2.18
+
+//exercise VAT 2
+const calculateBasePrice = function (priceIncludingVAT, VATPercentage) {
+    const basePrice = priceIncludingVAT / ((100 + VATPercentage) / 100);
+    return basePrice;
+};
+
+const calculateBasePriceAndVAT = function (priceIncludingVAT, VATPercentage) {
+    const basePrice = calculateBasePrice(priceIncludingVAT, VATPercentage);
+    const VAT = priceIncludingVAT - basePrice;
+    return [basePrice, VAT];
+};
+
+console.log(calculateBasePriceAndVAT(1210, 21)); // [1000, 210]
+console.log(calculateBasePriceAndVAT(2.18, 9)); // [2, 0.18]
