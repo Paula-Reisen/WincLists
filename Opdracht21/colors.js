43996568e7a7c25dcc79ed70019fd8973e5b785d
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2021-10-10 16:04:27 +0200

    Met solution

--- Opdracht21/colors.js
+++ Opdracht21/colors.js
@@ -14,3 +14,18 @@ colorArray.forEach(element => console.log(element));
 for (const [key, value] of Object.entries(colorArray)) {
     console.log(`${key}: ${value}`);
 }
+
+const myColors = {
+    colorWall: "blue",
+    colorFruit: "orange",
+    colorCar: "red",
+    colorHair: "white",
+    colorGras: "green",
+};
+
+for (x in myColors) {
+    console.log(myColors[x]);
+}
+
+// Ja, je bent nu aan het itereren. Echter ben je aan het itereren over een OBJECT.
+// Array Methods zijn NIET beschikbaar op een object.
