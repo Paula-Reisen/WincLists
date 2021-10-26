22f718f5499aed336a4b8bcafb6be40d7ac3cc8d
Paula-Reisen <36951747+Paula-Reisen@users.noreply.github.com>
2021-10-26 19:59:52 +0200

    Changes after feedback

--- Eindopdracht Javascript/Script.js
+++ Eindopdracht Javascript/Script.js
@@ -1,27 +1,35 @@
 const imgBtn = document.querySelector('img');
-const colorList = document.querySelector('li');
 const orangeBackground = document.querySelector('li.menu-orange');
 const redBackground = document.querySelector('li.menu-red');
 const blueBackground = document.querySelector('li.menu-blue');
 const purpleBackground = document.querySelector('li.menu-purple');
 const getBody = document.querySelector('body');
+const colorList = document.querySelector('ul');
+const removeColor = getBody.classList.remove('menu-orange', 'menu-red', 'menu-blue', 'menu-purple');
 
 imgBtn.addEventListener('click', function (e) {
-    const colorList = document.querySelector('ul');
     colorList.classList.remove('ul');
     colorList.classList.toggle('ul-active');
 });
 
 orangeBackground.addEventListener('click', function (e) {
-    getBody.classList.toggle('menu-orange');
+    removeColor;
+    getBody.classList.add('menu-orange');
+    colorList.classList.toggle('ul-active');
 });
 
 redBackground.addEventListener('click', function (e) {
-    getBody.classList.toggle('menu-red')
+    removeColor;
+    getBody.classList.add('menu-red');
+    colorList.classList.toggle('ul-active');
 });
 blueBackground.addEventListener('click', function (e) {
-    getBody.classList.toggle('menu-blue')
+    removeColor;
+    getBody.classList.add('menu-blue');
+    colorList.classList.toggle('ul-active');
 });
 purpleBackground.addEventListener('click', function (e) {
-    getBody.classList.toggle('menu-purple')
+    removeColor;
+    getBody.classList.add('menu-purple');
+    colorList.classList.toggle('ul-active');
 });
